c47c3dd3d41a6ba66806cd18bf66dda7a5c0ae29
HOParv <javierpescadornunez@gmail.com>
2026-05-29 18:13:48 +0200

    beta1

---
+++ 1.py
@@ -0,0 +1,11 @@
+import subprocess
+import os
+
+print("welcome tu linux auto-updater, please always run has sudo")
+
+while true:
+	if os.getuid != else:
+		print("execute has sudo using sudo python3")
+		break
+	if os.getuid != 0
+		

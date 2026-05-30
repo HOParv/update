a2f6b65d7a1f3684acdd3a48f644b01d1e33936d
HOParv <javierpescadornunez@gmail.com>
2026-05-30 16:13:46 +0200

    repair to the code

---
+++ README.md
@@ -0,0 +1,1 @@
+i've made this simple code on python to learn about subprocess library, you can use it to update your linux system if you don't like CLI
---
+++ update.py
@@ -0,0 +1,10 @@
+import os
+import subprocess
+
+print("WELCOME TO LINUX AUTO-UPDATER, PLEASE ALWAYS RUN AS SUDO")
+while True:
+    if os.getuid() != 0:
+        print("SUDO REQUIRED TO RUN AS SUDO")
+        break
+    elif os.getuid() == 0:
+        subprocess.call("apt update -y && apt upgrade -y", shell=True)

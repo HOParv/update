587f535abf0fd691a7f1986c1a54544be52392d4
HOParv <javierpescadornunez@gmail.com>
2026-06-11 09:17:19 +0200

    it has an installation script but doesn't works

---
+++ install/install.sh
@@ -0,0 +1,3 @@
+#mkdir /usr/local/bin/update
+sudo cp $HOME/update/update.py /usr/local/bin/update
+sudo cp $HOME/update/install/update /usr/local/bin/update/ 
--- update.py
+++ update.py
@@ -4,7 +4,7 @@ import subprocess
 print("WELCOME TO LINUX AUTO-UPDATER, PLEASE ALWAYS RUN AS SUDO")
 while True:
     if os.getuid() != 0:
-        print("SUDO REQUIRED TO RUN AS SUDO")
+        print("SUDO REQUIRED")
         break
     elif os.getuid() == 0:
         subprocess.call("apt update -y && apt upgrade -y", shell=True)
